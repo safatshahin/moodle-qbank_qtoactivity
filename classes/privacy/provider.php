72b7a674b42bde7ef579a0d8d850d1df8be40b69
Jhojji <kliddelled@gmail.com>
2022-09-01 10:55:50 +1000

    Add Privacy provider and coresponding string

---
+++ classes/privacy/provider.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace qbank_deletequestion\privacy;
+
+/**
+ * Privacy subsystem for qbank_q2activity that implements a null provider
+ *
+ * @package     qbank_q2activity
+ * @copyright   2022 Harrison Liddell, hliddell@myune.edu.au
+ * @copyright   Mark Hay,              mhay23@myune.edu.au
+ * @copyright   Henry Campbell,        hcampb25@myune.edu.au
+ * @copyright   Luke Purnell,          lpurnell@myune.edu.au
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
+    public static function get_reason(): string {
+        return 'privacy:metadata';
+    }
+}
